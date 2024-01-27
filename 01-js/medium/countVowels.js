86febfdd73b97d8da02688361d2c561248752146
Deepak Singhal <Deepaksinghal0320@gmail.com>
2024-01-27 16:33:18 +0530

    changes added

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,14 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  str1 = str.toLowerCase();
+  let vowelCount = 0;
+  for (let i = 0; i < str1.length; i++) {
+    console.log(str1[i]);
+    if(str1[i] === 'a' || str1[i] === 'e' || str1[i] === 'i'|| str1[i] === 'o'|| str1[i] === 'u');
+    vowelCount++;
+  }
+  return vowelCount;
 }
 
 module.exports = countVowels;
